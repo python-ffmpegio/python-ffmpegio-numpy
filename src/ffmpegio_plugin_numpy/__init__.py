94546f06e03317160a4119b32332be2db9d34eab
Takeshi Ikuma (LSUHSC) <tikuma@lsuhsc.edu>
2022-02-13 19:46:35 -0600

    fixed typing for python <v3.9

--- src/ffmpegio_plugin_numpy/__init__.py
+++ src/ffmpegio_plugin_numpy/__init__.py
@@ -2,6 +2,9 @@
 
 import numpy as np
 from pluggy import HookimplMarker
+from typing import Tuple
+
+from numpy.typing import ArrayLike
 
 hookimpl = HookimplMarker("ffmpegio")
 
@@ -16,84 +19,84 @@ __all__ = [
 
 
 @hookimpl
-def video_info(obj: object) -> tuple[tuple[int, int, int], str]:
+def video_info(obj: ArrayLike) -> Tuple[Tuple[int, int, int], str]:
     """get video frame info
 
-    :param obj: object containing video frame data with arbitrary number of frames
-    :type obj: object
-    :return: shape (height,width,components) and data type in numpy dtype str expression
-    :rtype: tuple[tuple[int, int, int], str]
+    :param obj: video frame data with arbitrary number of frames
+    :type obj: ArrayLike
+    :return: shape (height,width,components) and data type str
+    :rtype: Tuple[Tuple[int, int, int], str]
     """
     return obj.shape[-3:], obj.dtype.str
 
 
 @hookimpl
-def audio_info(obj: object) -> tuple[int, str]:
+def audio_info(obj: ArrayLike) -> Tuple[int, str]:
     """get audio sample info
 
-    :param obj: object containing audio data (with interleaving channels) with arbitrary number of samples
-    :type obj: object
-    :return: number of channels and sample data type in numpy dtype str expression
-    :rtype: tuple[tuple[int], str]
+    :param obj: column-wise audio data with arbitrary number of samples
+    :type obj: ArrayLike
+    :return: number of channels and sample data type in data type str
+    :rtype: Tuple[Tuple[int], str]
     """
     return obj.shape[-1:], obj.dtype.str
 
 
 @hookimpl
-def video_bytes(obj: object) -> memoryview:
-    """return bytes-like object of packed video pixels, associated with `video_info()`
+def video_bytes(obj: ArrayLike) -> memoryview:
+    """return bytes-like object of rawvideo NumPy array
 
-    :param obj: object containing video frame data with arbitrary number of frames
-    :type obj: object
-    :return: packed bytes of video frames
-    :rtype: bytes-like object
+    :param obj: video frame data with arbitrary number of frames
+    :type obj: ArrayLike
+    :return: memoryview of video frames
+    :rtype: memoryview
     """
 
     return memoryview(obj)
 
 
 @hookimpl
-def audio_bytes(obj: object) -> memoryview:
-    """return bytes-like object of packed audio samples
+def audio_bytes(obj: ArrayLike) -> memoryview:
+    """return bytes-like object of rawaudio NumPy array
 
-    :param obj: object containing audio data (with interleaving channels) with arbitrary number of samples
-    :type obj: object
-    :return: packed bytes of audio samples
-    :rtype: bytes-like object
+    :param obj: column-wise audio data with arbitrary number of samples
+    :type obj: ArrayLike
+    :return: memoryview of audio samples
+    :rtype: memoryview
     """
 
     return memoryview(obj)
 
 
 @hookimpl
-def bytes_to_video(b: bytes, dtype: str, shape: tuple[int, int, int]) -> object:
-    """convert bytes to rawvideo object
+def bytes_to_video(b: bytes, dtype: str, shape: Tuple[int, int, int]) -> ArrayLike:
+    """convert bytes to rawvideo NumPy array
 
     :param b: byte data of arbitrary number of video frames
     :type b: bytes
-    :param dtype: data type numpy dtype string (e.g., '|u1', '<f4')
+    :param dtype: data type string (e.g., '|u1', '<f4')
     :type dtype: str
     :param size: frame dimension in pixels and number of color components (height, width, components)
-    :type size: tuple[int, int, int]
-    :return: python object holding the rawvideo frames
-    :rtype: object
+    :type size: Tuple[int, int, int]
+    :return: rawvideo frames
+    :rtype: ArrayLike
     """
 
     return np.frombuffer(b, dtype).reshape(-1, *shape)
 
 
 @hookimpl
-def bytes_to_audio(b: bytes, dtype: str, shape: tuple[int]) -> object:
-    """convert bytes to rawaudio object
+def bytes_to_audio(b: bytes, dtype: str, shape: Tuple[int]) -> ArrayLike:
+    """convert bytes to rawaudio NumPy array
 
     :param b: byte data of arbitrary number of video frames
     :type b: bytes
-    :param dtype: numpy dtype string of the bytes (e.g., '<s2', '<f4')
+    :param dtype: data type string (e.g., '<s2', '<f4')
     :type dtype: str
-    :param shape: number of interleaved audio channels (1-element tuple)
-    :type shape: tuple[int]
-    :return: python object to hold the raw audio samples
-    :rtype: object
+    :param shape: number of audio channels
+    :type shape: Tuple[int]
+    :return: raw audio samples
+    :rtype: ArrayLike
     """
 
     return np.frombuffer(b, dtype).reshape(-1, *shape)
