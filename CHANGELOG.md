f529a6fa71f7f6397bdb8502d599955df9869627
Takeshi Ikuma (LSUHSC) <tikuma@lsuhsc.edu>
2022-02-13 19:30:28 -0600

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,16 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [1.0.0] - 2022-02-13
+
+### Added
+
+- First release.
+- A plugin to convert raw media I/O data of `ffmpegio` to use `numpy.ndarray` objects.
+
+[Unreleased]: https://github.com/python-ffmpegio/python-ffmpegio/compare/v1.0.0...HEAD
+
---
+++ tests/test_base.py
@@ -0,0 +1,63 @@
+import tempfile
+from os import path
+import numpy as np
+
+from ffmpegio import plugins, utils, audio, video
+
+
+def test_hooks():
+    hook = plugins.get_hook()
+
+    dtype = "|u1"
+    shape = (2, 2, 3)
+    b = b"\0" * utils.prod(shape)
+    data = hook.bytes_to_video(b=b, dtype=dtype, shape=shape)
+    assert memoryview(data).cast("b") == b
+    assert data.dtype.str == dtype
+    assert data.shape[1:] == shape
+    assert hook.video_info(obj=data) == (shape, dtype)
+    assert hook.video_bytes(obj=data).cast("b") == b
+
+    dtype = "<f4"
+    shape = (2,)
+    b = b"\0" * (1024 * utils.prod(shape))
+    data = hook.bytes_to_audio(b=b, dtype=dtype, shape=shape)
+    assert memoryview(data).cast("b") == b
+    assert data.dtype.str == dtype
+    assert data.shape[1:] == shape
+    assert hook.audio_info(obj=data) == (shape, dtype)
+    assert hook.audio_bytes(obj=data).cast("b") == b
+
+    print(plugins.pm.get_plugins())
+
+
+def test_audio():
+    fs, x = audio.create("anoisesrc", d=60, c="pink", a=0.5, sample_fmt="s16")
+    print(fs)
+    print(x.shape, x.dtype.str)
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        wavfile = path.join(tmpdirname, "test.wav")
+        audio.write(wavfile, fs, x)
+        fs1, x1 = audio.read(wavfile)
+
+    assert fs == fs1
+    assert np.array_equal(x, x1)
+
+
+def test_video():
+    fs, F = video.create("cellauto", p="@@ @ @@", s="100x400", full=0, rule=18, t_in=1)
+    print(fs)
+    print(F.shape, F.dtype.str)
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        avifile = path.join(tmpdirname, "test.avi")
+        video.write(avifile, fs, F, pix_fmt="bgr24", vcodec="rawvideo", show_log=True)
+        fs1, F1 = video.read(avifile)
+
+    assert fs == fs1
+    assert np.array_equal(F, F1)
+
+
+if __name__ == "__main__":
+    test_video()
